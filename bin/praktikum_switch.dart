9c503b10a22c7aa95f6ab56e2d4f568e0db19357
Anhar Tasman <anhartasman@JSN.JaringanKU>
2024-01-18 14:02:09 +0700

    praktikum switch

---
+++ bin/praktikum_switch.dart
@@ -0,0 +1,14 @@
+void main() {
+  String kendaraan = "Motor";
+  switch (kendaraan) {
+    case "Mobil":
+      print("4 roda");
+      break;
+    case "Motor":
+      print("2 roda");
+      break;
+    case "Bajai":
+      print("3 roda");
+      break;
+  }
+}
